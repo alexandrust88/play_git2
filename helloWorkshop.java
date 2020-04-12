6863e8a42d3bfc1e63bbe37d223c3e29265cd9d2
alex stan <alexandrust88@yahoo.com>
2020-04-12 10:58:47 +0300

    updates

--- helloWorkshop.java
+++ helloWorkshop.java
@@ -4,7 +4,7 @@ class helloWorkshop  {
     public static void main(String[] args) {
         System.out.println("Greetings Workshop!");
         System.out.println("This is an example file");
-        System.out.println("for use in workshop exercises.");
+        System.out.println("for use inexercises.");
         System.out.println("This is on the master branch");
         System.out.println("This file is written in Java.");
         System.out.println("Isn't this exciting?");
